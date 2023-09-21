7d61f51fd5e427e4929cddf5644bfa7706432763
Anselm Powell <anselm.powell@sport.wales>
2023-09-21 12:30:46 +0000

    Form validation

--- js/login_signup/login_signup.js
+++ js/login_signup/login_signup.js
@@ -1,95 +1,152 @@
-;
+
 
 // SIGN UP FORM
 
 
 // Creates a new account in the JSON file.
-function createAccount() {
+const createAccount = ()=> {
 
-    const localStorageKey = "accountData"
-    console.log("Create", "Enter")
-    // Get the username and password from the form.
-    const signupForm = document.querySelector("#signup-form");
-    const username = signupForm.querySelector("input[name='username']").value;
-    const password = signupForm.querySelector("input[name='password']").value;
-     
-    const jsonFile = JSON.parse(localStorage.getItem(localStorageKey)) || [];
-
-    if (!username) {
-        alert("Please Sign up");
-        return;
-    }
-
-    try{
-        if((jsonFile)) {
-            // Add the new account to the JSON file.
-            const userAccount = jsonFile.find(account => account.username === username);
-            if(userAccount.username === username) {
-                alert("Username Taken");
-                return
-            }
-            jsonFile.push({
-                username,
-                password,
-            });
-            window.location.href = "portal.html";
-        }
-    }catch {}
-
-    // Save the JSON file.
-    localStorage.setItem(localStorageKey, JSON.stringify(jsonFile));
-    console.log("succesful account creation")
-    signupForm.querySelector("input[name='password']").value = ""
-}
-
-
-// LOGIN FORM
-
-
-// Attempts to log the user in.
-function login() {
     
-    const localStorageKey = "accountData"
-    console.log("login", "Enter")
-    const loginForm = document.querySelector("#login-form");
-    // Get the username and password from the form.
-    const username = loginForm.querySelector("input[name='username']").value;
-    const password = loginForm.querySelector("input[name='password']").value;
-    loginForm.querySelector("input[name='password']").value = ""
-    // Get the JSON file.
-    const jsonFile = JSON.parse(localStorage.getItem(localStorageKey)) || [];
-
-  try{
-
-    if((jsonFile)) {
-        // Find the user's account in the JSON file.
-        console.log("accountData", jsonFile )
-        const userAccount = jsonFile.find(account => account.username === username);
-        // If the user's account doesn't exist, return an error message.
-
-        if (username === "") {
-            alert("Please enter login detials.");
-            return;
-        }
-        if (!userAccount) {
-            alert("Username or password is incorrect.");
-            console.log(userAccount.username, "User doest exist")
-            console.log(username)
-            return;
-        } 
-        // If the user's password doesn't match, return an error message.
-        if (userAccount.password !== password) {
-            alert("Username or password is incorrect.");
-            console.log(userAccount.password, "Password doest exist")
-            console.log(password)
-            return;
-        }
-    }
-        // If the user's login is successful, redirect them to the home page.
-        window.location.href = "portal.html";
-
-    } catch{
-    }
-  
-
+    // signupForm.querySelector("input[name='password']").value = ""
 }
+
+
+
+// LOGIN FORM AND VALIDATION
+(() => {
+    'use strict';
+  
+    // Fetch all DOM elements we need 
+    const login_forms = document.querySelectorAll('.login-validation');
+  
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(login_forms).forEach((form) => {
+      form.addEventListener('submit', (event) => {
+        // Fetch all DOM sign up form elements we need 
+        const loginForm = document.querySelector("#login-form");
+        const username = loginForm.querySelector("input[name='username']").value;
+        const password = loginForm.querySelector("input[name='password']").value;
+
+        document.querySelector(".invalid-user_pass").classList.remove('show')
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+          form.classList.add('was-validated');
+          return
+
+        }else{}{
+          event.preventDefault();
+
+          const localStorageKey = "accountData"
+          // ---uncomment to clear data ---
+          // JSON.parse(localStorage.clear(localStorageKey))  
+
+          // Get the JSON file.
+          const jsonFile = JSON.parse(localStorage.getItem(localStorageKey)) || [];
+
+      
+          try{
+
+            if((jsonFile)) {
+            // Find the user's account in the JSON file.
+            console.log("accountData", jsonFile )
+            const userAccount = jsonFile.find(account => account.username === username);
+            // If the user's account doesn't exist, return an error message.
+            if (!userAccount) {
+                document.querySelector(".invalid-user_pass").classList.add('show')
+                console.log(userAccount.username, "User doest exist")
+                console.log(username)
+                return;
+            } 
+            // If the user's password doesn't match, return an error message.
+            if (userAccount.password !== password) {
+
+                document.querySelector(".invalid-user_pass").classList.add('show')
+                console.log(userAccount.password, "Password doest exist")
+                console.log(password)
+                return;
+            }
+
+            if(userAccount && userAccount.password == password)
+            // If the user's login is successful, redirect them to the portal page.
+            window.location.href = "portal.html";
+      
+            }
+                
+            } catch{
+            }
+              }}, false);
+
+            });
+
+  
+  })();
+
+
+  // SIGNUP FORM AND VALIDATION
+  (() => {
+    'use strict';
+    const signup_forms = document.querySelectorAll('.signup-validation');
+  
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(signup_forms).forEach((form) => {
+      form.addEventListener('submit', (event) => {
+        // Fetch all DOM sign up form elements we need 
+        const signupForm = document.querySelector("#signup-form");
+        const username = signupForm.querySelector("input[name='username']").value;
+        const password = signupForm.querySelector("input[name='password']").value;
+
+        document.querySelector(".invalid-user_taken").classList.remove('show')
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+          form.classList.add('was-validated');
+        } else {
+          event.preventDefault();
+          console.log("successful enter")
+
+          const localStorageKey = "accountData"
+   
+          const jsonFile = JSON.parse(localStorage.getItem(localStorageKey)) || [];
+
+          try{
+              if((jsonFile)) {
+                console.log("successful enter 2", jsonFile)
+                  // Add the new account to the JSON file.
+                  const userAccount = jsonFile.find(account => account.username === username);
+                  console.log(userAccount)
+                  if(userAccount) {
+                      document.querySelector(".invalid-user_taken").classList.add('show')
+                      return
+                  }
+                  if(!username.trim().length) {
+                    console.log("no user")
+                    return
+                  }
+                  if(!password.trim().length) {
+                    console.log("no pass")
+                    return
+                  }
+                  console.log("pushh")
+                  jsonFile.push({
+                      username,
+                      password,
+                  });
+                  // Save the JSON file.
+                  localStorage.setItem(localStorageKey, JSON.stringify(jsonFile));
+                  console.log("successful account creation")
+                  window.location.href = "portal.html";
+                    }
+                }catch {}
+                
+
+
+
+        }
+        
+      }, false);
+    });
+  })();
+
